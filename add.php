a53c6b279ef477acf4d1306474cd98d3816f02a1
Emilio-W <emil.wibergh@gmail.com>
2023-01-20 08:08:55 +0100

    php3

--- add.php
+++ add.php
@@ -4,13 +4,14 @@
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
         $conn= mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E') or die("Connection Failed:" .mysqli_connect_error());
         if(isset($_POST['price']) && isset($_POST['rim']) && isset($_POST['gears']) && isset($_POST['brake']) && isset($_POST['available'])) {
-            $name= $_POST['price'];
-            $email= $_POST['rim'];
-            $phone= $_POST['gears'];
-            $bgroup= $_POST['brake'];
-            $bgroup= $_POST['available'];
 
-            $sql= "INSERT INTO `Bikes` (`price`, `rim`, `gears`, `brake`, `available`) VALUES ('$price', '$rim','$gears', '$brake', '$available')";
+            $price = $_POST['price'];
+            $rim = $_POST['rim'];
+            $gears = $_POST['gears'];
+            $brake = $_POST['brake'];
+            $available = $_POST['available'];
+
+            $sql = "INSERT INTO `Bikes` (`price`, `rim`, `gears`, `brake`, `available`) VALUES ('$price', '$rim','$gears', '$brake', '$available')";
 
             $query = mysqli_query($conn, $sql);
             if($query) {
