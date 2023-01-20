6f9e6f5ca76645b7fb22be56a9aa93ce43011720
Emilio-W <emil.wibergh@gmail.com>
2023-01-20 08:06:22 +0100

    php4

--- add.php
+++ add.php
@@ -3,7 +3,7 @@
     ini_set('display_errors', 1);
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
         $conn= mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E') or die("Connection Failed:" .mysqli_connect_error());
-        if(isset($_POST['price']) && isset($_POST['rim']) && isset($_POST['gears']) && isset($_POST['brake']) && isset($_POST['available']))) {
+        if(isset($_POST['price']) && isset($_POST['rim']) && isset($_POST['gears']) && isset($_POST['brake']) && isset($_POST['available'])) {
             $name= $_POST['price'];
             $email= $_POST['rim'];
             $phone= $_POST['gears'];
