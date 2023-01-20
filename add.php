bcbac966373ee2e42f0b2a8a3354352fdce117a6
Emilio-W <emil.wibergh@gmail.com>
2023-01-20 08:03:56 +0100

    php4

--- add.php
+++ add.php
@@ -1,14 +1,16 @@
 <?php 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-        $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E') or die("Connection Failed:" .mysqli_connect_error());
-        if(isset(isset($_POST['price']) && isset($_POST['rim']) && isset($_POST['gears']) && isset($_POST['brake']) && isset($_POST['available']))) {
-            $name = $_POST['price'];
-            $email = $_POST['rim'];
-            $phone = $_POST['gears'];
-            $bgroup = $_POST['brake'];
-            $bgroup = $_POST['available'];
+        $conn= mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E') or die("Connection Failed:" .mysqli_connect_error());
+        if(isset($_POST['price']) && isset($_POST['rim']) && isset($_POST['gears']) && isset($_POST['brake']) && isset($_POST['available']))) {
+            $name= $_POST['price'];
+            $email= $_POST['rim'];
+            $phone= $_POST['gears'];
+            $bgroup= $_POST['brake'];
+            $bgroup= $_POST['available'];
 
-            $sql = "INSERT INTO `Bikes` (`price`, `rim`, `gears`, `brake`, `available`) VALUES ('$price', '$rim','$gears', '$brake', '$available')";
+            $sql= "INSERT INTO `Bikes` (`price`, `rim`, `gears`, `brake`, `available`) VALUES ('$price', '$rim','$gears', '$brake', '$available')";
 
             $query = mysqli_query($conn, $sql);
             if($query) {
