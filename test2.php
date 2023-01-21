8fe325184bb82c6d7ead8757f6f5627d30abf4ba
Emilio-W <emil.wibergh@gmail.com>
2023-01-21 08:52:03 +0100

    php2

--- test2.php
+++ test2.php
@@ -1,13 +1,6 @@
 <?php
 $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 
-$first = $_POST['first'];
-$last = $_POST['last'];
-$pass = $_POST['pass'];
-
-$sql = "INSERT INTO test (first, last, pass) VALUES ('Emil', 'Wibergh', 'Stolt');";
+$sql = "INSERT INTO test (first, last, password) VALUES ('Emil', 'Wibergh', 'Stolt');";
 mysqli_query($conn, $sql);
-
-#header("Location: test.php?signup=success");
-echo 'fu';
 ?>
