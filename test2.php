19b34caa3e7ca11e10c7a1c8dffbba9365043c9c
Emilio-W <emil.wibergh@gmail.com>
2023-01-21 08:51:02 +0100

    php2

--- test2.php
+++ test2.php
@@ -1,11 +1,11 @@
 <?php
-$conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'test');
+$conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 
 $first = $_POST['first'];
 $last = $_POST['last'];
 $pass = $_POST['pass'];
 
-$sql = "INSERT INTO users (first, last, pass) VALUES ('Emil', 'Wibergh', 'Stolt');";
+$sql = "INSERT INTO test (first, last, pass) VALUES ('Emil', 'Wibergh', 'Stolt');";
 mysqli_query($conn, $sql);
 
 #header("Location: test.php?signup=success");
