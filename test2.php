2cc141f16dd191d60945506a317aeeeaa728a5b3
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 05:23:06 +0100

    php

--- test2.php
+++ test2.php
@@ -10,11 +10,9 @@ $result = mysqli_query($conn, $sql);
 $resultcheck = mysqli_num_rows($result);
 echo $resultcheck;
 
-if ($resultCheck > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
 
 		echo $row;
 					
 	}
-}
 ?>
