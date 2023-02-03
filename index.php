896c5f1e61db47e48b267b42e6ea48f08c13adce
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 07:36:23 +0100

    php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 	<body>
 		<div class="navbar">Luleå Bikes</div>
 		<div class="main">
+			<div class="bookSuccess">Your booking was Successful!</div>
 			<div class="avbikes">
 			<?php
 			$sql = 'SELECT * FROM Bikes WHERE status="available";';
