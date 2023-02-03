b11341ba264e8deb0dd5412ab78134432014a6e9
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 06:52:57 +0100

    php

--- bookbike.php
+++ bookbike.php
@@ -15,21 +15,21 @@ $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
             <h1>Booking of Bike #<?php echo $_POST['id']?></h1>
             <form action = 'addBooking.php' method ="POST">
 
-                <input type="hidden" name="id"  value="<?php echo $_POST['id']?>" readonly/> <br> <br>
+                <input type="hidden" name="id"  value="<?php echo $_POST['id']?>" readonly required/> <br> <br>
 
-                <input type="text" name="startDate"  placeholder="Start Date"/> <br> <br>
+                <input type="text" name="startDate"  placeholder="Start Date" required/> <br> <br>
 
-                <input type="text" name="endDate" placeholder="End Date"/> <br> <br>
+                <input type="text" name="endDate" placeholder="End Date" required/> <br> <br>
 
-                <input type="text" name="pickupPoint"  placeholder="Pickup Point"/> <br> <br>
+                <input type="text" name="pickupPoint"  placeholder="Pickup Point" required/> <br> <br>
 
-                <input type="text" name="returnPoint"  placeholder="Return Point"/> <br> <br>
+                <input type="text" name="returnPoint"  placeholder="Return Point" required/> <br> <br>
 
-                <input type="text" name="bookedBy"  placeholder="Booked By"/> <br> <br>
+                <input type="text" name="bookedBy"  placeholder="Booked By" required/> <br> <br>
 
-                <input type="text" name="bikeBooked"  placeholder="Bike Booked"/> <br> <br>
+                <input type="text" name="bikeBooked"  placeholder="Bike Booked" required/> <br> <br>
 
-                <input type="text" name="returned"  placeholder="Returned"/> <br> <br>
+                <input type="text" name="returned"  placeholder="Returned" required/> <br> <br>
 
                 <button type="submit" name="submit">Book Bike</button>
             </form>
