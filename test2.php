7dcf53a220bf47efdd92de977f7d1992e1db9ce6
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 05:17:14 +0100

    php

--- test2.php
+++ test2.php
@@ -1,8 +1,8 @@
 <?php
 $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 
-// $sql = "INSERT INTO Bikes (status, price, rim, gears, brake) VALUES ('available', '300', '2', '1', '1');";
-// mysqli_query($conn, $sql);
+$sql = "INSERT INTO Bikes (status, price, rim, gears, brake) VALUES ('available', '300', '2', '1', '1');";
+mysqli_query($conn, $sql);
 
 
 $sql = "SELECT * FROM Bikes";
