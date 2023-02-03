e740568175960f8596cde24d70fa5ee5cc8a4140
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 05:16:35 +0100

    php

--- test2.php
+++ test2.php
@@ -8,6 +8,7 @@ $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 $sql = "SELECT * FROM Bikes";
 $result = mysqli_query($conn, $sql);
 $resultcheck = mysqli_num_rows($result);
+echo $resultcheck;
 
 if ($resultCheck > 0) {
 	while ($row = mysqli_fetch_assoc($result)) {
