514679bd0c48485ea644ee96e8c5354332a72c9d
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 07:56:57 +0100

    php

--- addBooking.php
+++ addBooking.php
@@ -10,9 +10,19 @@ $bookedBy = $_POST['bookedBy'];
 $bikeBooked = $_POST['bikeBooked'];
 $returned = $_POST['returned'];
 
-$sql = "UPDATE Bikes SET status='Booked' WHERE id=$id";
+// check if bike is still available
+$sql = "SELECT * FROM products where id = $id";
+$result = mysqli_query($conn, $sql);
+if ($result = mysqli_num_rows($result) == 0) {
+    header("Location: index.php?booking=failed");
+}
+
+
+// change bike availability to booked
+$sql = "UPDATE Bikes SET status='booked' WHERE id=$id";
 mysqli_query($conn, $sql);
 
+// insert booking to bookings database
 $sql = "INSERT INTO bookings (startDate, endDate, pickupPoint, returnPoint, bookedBy, bikeBooked, returned) VALUES ('$startDate', '$endDate', '$pickupPoint', '$returnPoint', '$bookedBy', '$bikeBooked', '$returned')";
 mysqli_query($conn, $sql);
 
