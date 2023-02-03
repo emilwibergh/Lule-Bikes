2e0042998e98625de375050d2d1eff2787deddf7
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 04:27:41 +0100

    php

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
 	<body>
 
 		<div class="main">
+			<?php
+				echo '<div class="avbikes">';
+					echo '<div class="bikeAd" style="background-image: url(BikeAdImages/001.jpg);">';
+					echo '<div class="bikeAdID">#001</div>';
+					echo '<div class="bikeAdBar"> </div>';
+					echo '<div class="bikeAdPrice">500 kr</div>';
+					echo '<div class="bikeAdButton">Rent</div>';
+				echo '</div>';
+			?>
 			<div class="avbikes">
 				<div class="bikeAd" style="background-image: url(BikeAdImages/001.jpg);">
 					<div class="bikeAdID">#001</div>
