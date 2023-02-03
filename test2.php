f65b1e110bee8b37038064227c77af567781a895
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 05:14:07 +0100

    php

--- test2.php
+++ test2.php
@@ -1,19 +1,15 @@
 <?php
 $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 
-$sql = "INSERT INTO Bikes (status, price, rim, gears, brake) VALUES ('available', '300', '2', '1', '1');";
-mysqli_query($conn, $sql);
+$sql = "SELECT * FROM Bikes";
+$result = mysqli_query($conn, $sql);
+$resultcheck = mysqli_num_rows($result);
 
+if ($resultCheck > 0) {
+	while ($row = mysqli_fetch_assoc($result)) {
 
-$sql = 'SELECT * FROM Bikes';
-			$result = mysqli_query($conn, $sql);
-			$resultcheck = mysqli_num_rows($result);
-
-			if ($resultCheck > 0) {
-				while ($row = mysqli_fetch_assoc($result)) {
-
-					echo $row['price'] . "<br>";
+		echo $row['price'];
 					
-				}
-			}
+	}
+}
 ?>
