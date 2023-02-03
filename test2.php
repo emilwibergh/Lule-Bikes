b5dd4472e054cc0b9b5cc69438b10cf27de68f79
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 05:00:17 +0100

    php

--- test2.php
+++ test2.php
@@ -3,4 +3,17 @@ $conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
 
 $sql = "INSERT INTO Bikes (status, price, rim, gears, brake) VALUES ('available', '300', '2', '1', '1');";
 mysqli_query($conn, $sql);
+
+
+$sql = 'SELECT * FROM Bikes WHERE status="available";';
+			$result = mysqli_query($conn, $sql);
+			$resultcheck = mysqli_num_rows($result);
+
+			if ($resultCheck > 0) {
+				while ($row = mysqli_fetch_assoc($result)) {
+
+					echo $row['id'] . "<br>";
+					
+				}
+			}
 ?>
