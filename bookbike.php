12e4efb776b8f20f9cda81aee91cd8946199b657
Emilio-W <emil.wibergh@gmail.com>
2023-02-03 06:50:21 +0100

    php

--- bookbike.php
+++ bookbike.php
@@ -1,28 +1,37 @@
+<?php
+$conn = mysqli_connect('localhost', 'D0018E', 'D0018E', 'D0018E');
+?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset ="UTF-8">
-    </head>
-    <body>
-        <form action = 'addBooking.php' method ="POST">
+	<head>
+		<title>Luleå Bikes</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+	</head>
+	<body>
+		<div class="navbar">Luleå Bikes</div>
+		<div class="main">
+            <form action = 'addBooking.php' method ="POST">
 
-            <input type="text" name="id"  value="<?php echo $_POST['id']?>" readonly/> <br> <br>
+                <input type="text" name="id"  value="<?php echo $_POST['id']?>" readonly/> <br> <br>
 
-            <input type="text" name="startDate"  placeholder="Start Date"/> <br> <br>
+                <input type="text" name="startDate"  placeholder="Start Date"/> <br> <br>
 
-            <input type="text" name="endDate" placeholder="End Date"/> <br> <br>
+                <input type="text" name="endDate" placeholder="End Date"/> <br> <br>
 
-            <input type="text" name="pickupPoint"  placeholder="Pickup Point"/> <br> <br>
+                <input type="text" name="pickupPoint"  placeholder="Pickup Point"/> <br> <br>
 
-            <input type="text" name="returnPoint"  placeholder="Return Point"/> <br> <br>
+                <input type="text" name="returnPoint"  placeholder="Return Point"/> <br> <br>
 
-            <input type="text" name="bookedBy"  placeholder="Booked By"/> <br> <br>
+                <input type="text" name="bookedBy"  placeholder="Booked By"/> <br> <br>
 
-            <input type="text" name="bikeBooked"  placeholder="Bike Booked"/> <br> <br>
+                <input type="text" name="bikeBooked"  placeholder="Bike Booked"/> <br> <br>
 
-            <input type="text" name="returned"  placeholder="Returned"/> <br> <br>
+                <input type="text" name="returned"  placeholder="Returned"/> <br> <br>
 
-            <button type="submit" name="submit">Book Bike</button>
-        </form>
-    </body>
+                <button type="submit" name="submit">Book Bike</button>
+            </form>
+		</div>
+	</body>
 </html>
