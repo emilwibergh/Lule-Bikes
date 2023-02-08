7be6d93b94c2c2e44d82edb51c6696489fcdf960
IMJEM <114428027+IMJEM@users.noreply.github.com>
2023-02-08 17:02:31 +0100

    Create config.php
    connecting to the mysql db

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "pwd";
+$db = "namn" ;
+
+//create connection to mysql db
+$conn = new mysqli($host, $username, $password, $db);
+
+//checkar om den är connected
+
+if (!$conn) {
+    die( " Connection failed: " . mysqli_connect_error ());
+}
+// echo " Connected successfully" ;
+
+?>
